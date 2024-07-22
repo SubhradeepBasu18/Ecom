4f7999d7b37c2b7be8e710eebbbe3167466a7435
Subhradeep <subhradeepbasu2002@gmail.com>
2024-07-22 10:47:18 +0530

    Update Appwrite configuration to use separate config file

--- src/Appwrite/config.js
+++ src/Appwrite/config.js
@@ -1,10 +1,11 @@
 import { Client, Account, Databases,ID } from "appwrite";
+import conf from "../conf/conf.js"
 
 const client = new Client();
 
-client.setEndpoint(String(import.meta.env.VITE_APPWRITE_URL))
-      .setProject(String(import.meta.env.VITE_APPWRITE_PROJECT))
+client.setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId)
 
 export const account = new Account(client) 
 
-export const database = new Databases(client,String(import.meta.env.VITE_APPWRITE_DATABASE))
+export const database = new Databases(client,conf.appwriteDatabaseId)
---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+}
+
+console.log("Appwrite URL:", conf.appwriteUrl);
+console.log("Appwrite Project ID:", conf.appwriteProjectId);
+console.log("Appwrite Database ID:", conf.appwriteDatabaseId);
+
+export default conf;
